cfbd4a33b6326ca2f688260ffb038e3475a77836
hahazexia <hahazexia@outlook.com>
2025-08-22 11:29:52 +0800

    feat: migration sql script avoid non-constant default error

--- src/main/db/migrations.ts
+++ src/main/db/migrations.ts
@@ -1,16 +1,31 @@
 import sqlite from 'better-sqlite3';
+import { formatLocalTime } from '../utils.js';
 
 const migrations = [
   {
     version: '0.0.19',
     up: (db: sqlite.Database) => {
+      // if already have data in table
+      // ALTER TABLE configs ADD COLUMN create_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')) will cause error below
+      // Cannot add a column with non-constant default
+      // db.exec(`
+      //     ALTER TABLE configs
+      //     ADD COLUMN create_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+      //   `);
+      // db.exec(`
+      //     ALTER TABLE configs
+      //     ADD COLUMN update_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+      //   `);
+      const now: string = formatLocalTime();
+      global.log.info(`now: ${now}`);
+
       db.exec(`
           ALTER TABLE configs
-          ADD COLUMN create_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+          ADD COLUMN create_at TEXT NOT NULL DEFAULT '${now}'
         `);
       db.exec(`
           ALTER TABLE configs
-          ADD COLUMN update_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+          ADD COLUMN update_at TEXT NOT NULL DEFAULT '${now}'
         `);
     },
   },
